3305caf13175d3b55b21f5ff88ec5f352fc97867
fabiomirandafig <fabiomirandafig@gmail.com>
2020-12-12 17:15:51 -0300

    QR_Code

---
+++ src/com.chillyfacts.com/com/chillyfacts/com/com/chillyfacts/com/Create_QR.java
@@ -0,0 +1,41 @@
+package com.chillyfacts.com;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter; 
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+public class Create_QR {
+    public static void main(String[] args) {
+        try {
+            String qrCodeData = Alfanumerico();
+            String filePath = "/home/fabio/Música/codigodepontos.png";
+            String charset = "UTF-8"; // or "ISO-8859-1"
+            Map < EncodeHintType, ErrorCorrectionLevel > hintMap = new HashMap < EncodeHintType, ErrorCorrectionLevel > ();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            BitMatrix matrix = new MultiFormatWriter().encode(
+                new String(qrCodeData.getBytes(charset), charset),
+                BarcodeFormat.QR_CODE, 1000, 1000, hintMap);
+            MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
+                .lastIndexOf('.') + 1), new File(filePath));
+            System.out.println("QR Code image created successfully!");
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public static String Alfanumerico(){
+        String[] caracteres ={"0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+
+        String codigo="";
+        for (int x=0; x<40; x++){
+            int c = (int) (Math.random()*caracteres.length);
+            codigo += caracteres[c];
+        }
+        System.out.println("O código alfanumérico gerado é: "+codigo);   
+        return codigo;
+    }
+}
